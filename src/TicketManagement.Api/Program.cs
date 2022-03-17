107bb31f4a1d94cd5257302f4e6abddf2176f4ad
Igor Castro <igorcastro.dias@gmail.com>
2022-03-17 00:32:50 -0300

    Configuring Api project

--- src/TicketManagement.Api/Program.cs
+++ src/TicketManagement.Api/Program.cs
@@ -1,6 +1,8 @@
+using TicketManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddPersistenceServices(builder.Configuration);
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
