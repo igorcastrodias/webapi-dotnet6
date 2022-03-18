5883d6238a012a36b8cd705a1fb79218ce0c5f59
Igor Castro <igorcastro.dias@gmail.com>
2022-03-17 21:58:42 -0300

    Created controllers api project

--- src/TicketManagement.Api/Program.cs
+++ src/TicketManagement.Api/Program.cs
@@ -1,8 +1,31 @@
+using TicketManagement.Application;
 using TicketManagement.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-builder.Services.AddPersistenceServices(builder.Configuration);
+services.AddApplicationServices();
+services.AddPersistenceServices(builder.Configuration);
+services.AddControllers();
+
+services.AddCors(options => 
+{
+    options.AddPolicy("Open",builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
+var env = builder.Environment;
 
 var app = builder.Build();
+
+if(env.IsDevelopment()){
+    app.UseDeveloperExceptionPage();
+}
+app.UseRouting();
+
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 app.Run();
